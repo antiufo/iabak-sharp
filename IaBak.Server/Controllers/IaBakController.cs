347ddb69579342366beb86531719cde43a0b88ad
Andrea Martinelli <martinelli-andrea@outlook.com>
2020-07-03 20:02:03 +0200

    Initial working implementation of server

--- IaBak.Server/Controllers/IaBakController.cs
+++ IaBak.Server/Controllers/IaBakController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using IaBak.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace IaBak.Server.Controllers
@@ -24,11 +25,80 @@ namespace IaBak.Server.Controllers
         }
 
 
-
-        [HttpGet]
-        public IEnumerable<User> Get()
+        [HttpPost("CheckServerStatus")]
+        public Task<CheckServerStatusResponse> CheckServerStatus(CheckServerStatusRequest request)
         {
-            return _dbContext.Users.ToList();
+            return Task.FromResult(new CheckServerStatusResponse());
+        }
+
+
+        private async Task<User> GetUserAsync(RequestBase request)
+        {
+            var user = await _dbContext.Users.FirstAsync(x => x.UserId == request.UserId && x.SecretKey == request.SecretKey);
+            if (user == null) throw new Exception("Invalid user.");
+            return user;
+        }
+
+        private async Task<long> GetUserIdAsync(RequestBase request) => (await GetUserAsync(request)).UserId;
+
+        [HttpPost("Sync")]
+        public async Task<SyncResponse> Sync(SyncRequest request)
+        {
+            var now = DateTime.UtcNow;
+            var user = await GetUserAsync(request);
+
+            foreach (var itemId in request.GainedItems)
+            {
+                var info = await _dbContext.TryGetItemStorageAsync(user.UserId, itemId);
+
+                if (info != null)
+                {
+                    _dbContext.ItemStorage.Update(info);
+                }
+                else
+                {
+                    info = new ItemStorage { ItemId = itemId, UserId = user.UserId, DateNotified = now };
+                    _dbContext.ItemStorage.Add(info);
+                }
+            }
+
+            foreach (var itemId in request.LostItems)
+            {
+                var info = await _dbContext.TryGetItemStorageAsync(user.UserId, itemId);
+
+                if (info != null)
+                    _dbContext.ItemStorage.Remove(info);
+            }
+
+            user.LastSync = DateTime.UtcNow;
+            
+            await _dbContext.SaveChangesAsync();
+            return new SyncResponse();
+
+        }
+
+        [HttpPost("JobRequest")]
+        public async Task<JobRequestResponse> JobRequest(JobRequestRequest request)
+        {
+            var suggestion = await _dbContext.ArchiveItems
+                .Where(x => x.CurrentRedundancy == 0 && !_dbContext.RecentSuggestions.Any(y => y.ItemId == x.Identifier))
+                .FirstOrDefaultAsync();
+
+            var user = await GetUserAsync(request);
+            if (suggestion != null)
+            {
+                _dbContext.RecentSuggestions.Add(new RecentSuggestion { ItemId = suggestion.Identifier, UserId = user.UserId, SuggestionDate = DateTime.UtcNow });
+            }
+
+            await _dbContext.SaveChangesAsync();
+            return new JobRequestResponse
+            {
+                RetryInSeconds = suggestion != null ? 10 : 60,
+                Suggestions = suggestion != null ? new List<ItemDownloadSuggestion> {
+                    new ItemDownloadSuggestion { ItemName = suggestion.Identifier }
+                } : null
+            };
+            
         }
 
         [HttpPost("Registration")]
--- IaBak.Server/Models/IaBakDbContext.cs
+++ IaBak.Server/Models/IaBakDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -13,11 +15,21 @@ namespace IaBak.Server
         public DbSet<User> Users { get; set; }
         public DbSet<ArchiveItem> ArchiveItems { get; set; }
         public DbSet<ItemStorage> ItemStorage { get; set; }
+        public DbSet<RecentSuggestion> RecentSuggestions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ItemStorage>()
                 .HasKey(c => new { c.ItemId, c.UserId });
+            modelBuilder.Entity<RecentSuggestion>()
+                .HasKey(c => new { c.ItemId, c.UserId });
+        }
+
+
+
+        public async Task<ItemStorage> TryGetItemStorageAsync(long userId, string itemId)
+        {
+            return await ItemStorage.FirstOrDefaultAsync(x => x.UserId == userId && x.ItemId == itemId);
         }
     }
 
@@ -36,13 +48,22 @@ namespace IaBak.Server
     public class ItemStorage
     {
         public long UserId { get; set; }
-        public long ItemId { get; set; }
+        public string ItemId { get; set; }
+        public DateTime DateNotified { get; set; }
+    }
+
+    public class RecentSuggestion
+    {
+        public long UserId { get; set; }
+        public string ItemId { get; set; }
+        public DateTime SuggestionDate { get; set; }
     }
 
     public class ArchiveItem
     {
-        [Key] public long ItemId { get; set; }
-        public string Identifier { get; set; }
-        public long? TotalSize { get; set; }
+        [Key] public string Identifier { get; set; }
+        public long TotalSize { get; set; }
+        public int CurrentRedundancy { get; set; }
+        public double Priority { get; set; }
     }
 }
